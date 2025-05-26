be823f541eceee32d047d7e9e5ba430fb37c0e9d
Matthias Morin <mat@tangoman.io>
2025-05-26 11:57:14 +0200

    🎉 Initial Commit

---
+++ devops/install_google-cloud-sdk.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## install google-cloud-sdk
+##
+## @category devops
+## @link     https://docs.gcloud.com
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+# --------------------------------------------------
+
+alert_primary 'Install google-cloud-sdk'
+
+if [ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]; then
+    # Install repository
+    echo_info 'echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list\n'
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+fi
+
+if [ ! -f /usr/share/keyrings/cloud.google.gpg ]; then
+    # Install key
+    echo_info 'curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -\n'
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+fi
+
+echo_info 'sudo apt-get update\n'
+sudo apt-get update
+
+echo_info 'sudo apt-get install --assume-yes apt-transport-https\n'
+sudo apt-get install --assume-yes apt-transport-https
+
+echo_info 'sudo apt-get install --assume-yes ca-certificates gnupg\n'
+sudo apt-get install --assume-yes ca-certificates gnupg
+
+echo_info 'sudo apt-get install --assume-yes google-cloud-sdk\n'
+sudo apt-get install --assume-yes google-cloud-sdk
+
+#--------------------------------------------------
+# optional
+#--------------------------------------------------
+
+echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-gke-gcloud-auth-plugin\n'
+sudo apt-get install --assume-yes google-cloud-sdk-gke-gcloud-auth-plugin
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-app-engine-python\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-app-engine-python
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-app-engine-python-extras\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-app-engine-python-extras
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-app-engine-java\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-app-engine-java
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-app-engine-go\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-app-engine-go
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-bigtable-emulator\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-bigtable-emulator
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-cbt\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-cbt
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-cloud-build-local\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-cloud-build-local
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-datalab\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-datalab
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-datastore-emulator\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-datastore-emulator
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-firestore-emulator\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-firestore-emulator
+
+# echo_info 'sudo apt-get install --assume-yes google-cloud-sdk-pubsub-emulator\n'
+# sudo apt-get install --assume-yes google-cloud-sdk-pubsub-emulator
+
+# echo_info 'sudo apt-get install --assume-yes kubectl\n'
+# sudo apt-get install --assume-yes kubectl
---
+++ themes/remove_bootloader_theme.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## TangoMan Bootloader Theme Remover
+##
+## @license MIT
+## @author  "Matthias Morin" <mat@tangoman.io>
+## @category themes
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+# --------------------------------------------------
+
+THEME_DIR=/usr/share/grub/themes
+GRUB_CONFIG_FILE=/etc/default/grub
+
+function remove_themes() {
+    echo_warning "Removing theme folder...\n"
+
+    echo_info "sudo rm -rf \"${THEME_DIR}\"\n"
+    sudo rm -rf "${THEME_DIR}"
+
+    echo_info "sudo mkdir -p \"${THEME_DIR}\"\n"
+    sudo mkdir -p "${THEME_DIR}"
+}
+
+function config_grub() {
+    echo_success 'Setting grub timeout to 0 second\n'
+
+    # remove default timeout if any
+    echo_info "sudo sed -i '/GRUB_TIMEOUT=/d' \"${GRUB_CONFIG_FILE}\"\n"
+    sudo sed -i '/GRUB_TIMEOUT=/d' "${GRUB_CONFIG_FILE}"
+
+    echo_info "sudo bash -c \"echo 'GRUB_TIMEOUT=\"0\"' >> \"${GRUB_CONFIG_FILE}\"\"\n"
+    sudo bash -c "echo 'GRUB_TIMEOUT=\"0\"' >> \"${GRUB_CONFIG_FILE}\""
+
+    echo_success "Remove theme configuration"
+    # remove theme if any
+    echo_info "sudo sed -i '/GRUB_THEME=/d' \"${GRUB_CONFIG_FILE}\"\n"
+    sudo sed -i '/GRUB_THEME=/d' "${GRUB_CONFIG_FILE}"
+}
+
+function update_grub() {
+    # Update grub config
+    echo_success 'Updating grub config...\n'
+    if [[ -x "$(command -v update-grub)" ]]; then
+        echo_info 'sudo update-grub\n'
+        sudo update-grub
+
+    elif [[ -x "$(command -v grub-mkconfig)" ]]; then
+        echo_info 'sudo grub-mkconfig -o /boot/grub/grub.cfg\n'
+        sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+    elif [[ -x "$(command -v grub2-mkconfig)" ]]; then
+        if [[ -x "$(command -v zypper)" ]]; then
+            echo_info 'sudo grub2-mkconfig -o /boot/grub2/grub.cfg\n'
+            sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+        elif [[ -x "$(command -v dnf)" ]]; then
+            echo_info 'sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg\n'
+            sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+        fi
+    fi
+}
+
+function main() {
+    alert_danger 'TangoMan Theme Unnstaller'
+
+    remove_themes
+
+    config_grub
+    update_grub
+}
+
+main
