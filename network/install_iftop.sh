cd79fe5f4def67e196666ca3f867de3be609b09d
Matthias Morin <mat@tangoman.io>
2025-06-26 15:09:26 +0200

    🎉 Initial Commit

---
+++ network/install_iftop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## iftop
+## display bandwidth usage on an interface by host
+##
+## ```
+## # list network devices
+## lshw -short -c network
+##
+## # print network traffic
+## sudo iftop -i wlp2s0
+## ```
+##
+## @category network
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+# --------------------------------------------------
+
+alert_primary 'Install iftop'
+
+echo_info 'sudo apt-get install --assume-yes iftop\n'
+sudo apt-get install --assume-yes iftop
---
+++ network/remove_postman.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## postman
+##
+## @category network
+## @link     https://dbeaver.io
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+# --------------------------------------------------
+
+APP_NAME=postman
+
+alert_danger "Remove ${APP_NAME}"
+
+if [ ! -x "$(command -v snap)" ]; then
+    echo_danger "error: \"$(basename "${0}")\" requires snap to install, try: 'sudo apt-get install -y snapd'\n"
+    exit 1
+fi
+
+# remove with snap
+echo_info "sudo snap remove \"${APP_NAME}\"\n"
+sudo snap remove "${APP_NAME}"
+
+# remove desktop shortcut
+echo_info "rm \"$(xdg-user-dir DESKTOP)\"/\"${APP_NAME}\".desktop\n"
+rm "$(xdg-user-dir DESKTOP)"/"${APP_NAME}".desktop
