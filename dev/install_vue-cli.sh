ce6527eb4c9b3bfb7d728b095eae1a4349132f65
Matthias Morin <mat@tangoman.io>
2025-11-26 11:23:17 +0100

    🎉 Initial Commit

---
+++ dev/install_vue-cli.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## vue-cli
+##
+## @category dev
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+#--------------------------------------------------
+
+alert_primary 'Install vue-cli'
+
+if [ ! -x "$(command -v npm)" ] && [ ! -x "$(command -v yarn)" ]; then
+    echo_danger "error: \"$(basename "${0}")\" requires yarm, try: 'sudo apt-get install -y yarm'\n"
+    exit 1
+fi
+
+if [ -x "$(command -v yarn)" ]; then
+    echo_info 'sudo yarn global add @vue/cli\n'
+    sudo yarn global add @vue/cli
+
+    echo_info 'sudo yarn global add @vue/cli-init\n'
+    sudo yarn global add @vue/cli-init
+
+    exit 0
+fi
+
+if [ -x "$(command -v npm)" ]; then
+    echo_info 'sudo npm install -g @vuecli\n'
+    sudo npm install -g @vuecli
+
+    exit 0
+fi
+
---
+++ templates/remove_template_apt.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2025 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## remove template
+##
+## @category template
+## @link     https://github.com/TangoMan75/template
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+#--------------------------------------------------
+
+APP_NAME=template
+
+#--------------------------------------------------
+
+alert_danger "Remove ${APP_NAME}"
+
+#--------------------------------------------------
+
+echo_info "sudo apt-get remove -y \"${APP_NAME}\"\n"
+sudo apt-get remove -y "${APP_NAME}"
+
+#--------------------------------------------------
+
+# remove desktop shortcut
+echo_info "rm \"$(xdg-user-dir DESKTOP)\"/\"${APP_NAME}\".desktop\n"
+rm "$(xdg-user-dir DESKTOP)"/"${APP_NAME}".desktop
