cef5e5dfcdf3c207bb57b8cf9e36abca255781b2
Matthias Morin <mat@tangoman.io>
2026-09-18 20:57:27 +0200

    wip: 2026-09-18 20:57:26

---
+++ communication/remove_slack.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+## This file is part of TangoMan Provisions package.
+##
+## Copyright (c) 2026 "Matthias Morin" <mat@tangoman.io>
+##
+## This source file is subject to the MIT license that is bundled
+## with this source code in the file LICENSE.
+
+## slack
+##
+## @category communication
+## @link     https://slack.com/downloads/linux
+## @link     https://slack.com/downloads/instructions/linux?ddl=1&build=deb
+
+CURDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+# shellcheck source=/dev/null
+. "${CURDIR}/../tools/src/colors/colors.sh"
+
+#--------------------------------------------------
+
+APP_NAME=slack
+PACKAGE_NAME=slack-desktop
+
+#--------------------------------------------------
+
+_alert_danger "Remove ${APP_NAME}"
+
+#--------------------------------------------------
+
+# Check for required commands
+REQUIRED_COMMANDS=(dpkg)
+for cmd in "${REQUIRED_COMMANDS[@]}"; do
+    if ! command -v "${cmd}" &> /dev/null; then
+        _echo_danger "error: \"$(basename "${0}")\" requires ${cmd}, try: 'sudo apt-get install -y ${cmd}'\n"
+        exit 1
+    fi
+done
+
+#--------------------------------------------------
+
+_echo_info "sudo dpkg --purge \"${PACKAGE_NAME}\"\n"
+sudo dpkg --purge "${PACKAGE_NAME}"
+
+#--------------------------------------------------
+
+# remove config files
+_echo_info "rm -rf \"${HOME}/.config/Slack\"\n"
+rm -rf "${HOME}/.config/Slack"
+
+#--------------------------------------------------
+
+# remove desktop shortcut
+_echo_info "rm -f \"$(xdg-user-dir DESKTOP)/${APP_NAME}.desktop\"\n"
+rm -f "$(xdg-user-dir DESKTOP)/${APP_NAME}.desktop"
